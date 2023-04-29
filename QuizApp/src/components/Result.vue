14d03575501eed2ee07d6801100d929bbacdf916
carllrac1 <cristianlopez.crl@gmail.com>
2023-04-28 23:12:39 -0600

    Propiedades computadas y emits

---
+++ QuizApp/src/components/Result.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+    import { defineProps } from 'vue'
+
+    const { quizQuestionLength, numberOfCorrectAnswers } = defineProps([
+        "quizQuestionLength",
+        "numberOfCorrectAnswers"
+    ])
+
+</script>
+
+<template>
+    <div class="results">
+        <p>Your results...</p>
+        <h1>{{ numberOfCorrectAnswers }} / {{ quizQuestionLength }}</h1>
+        <RouterLink to="/">Go back</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+
+.results {
+    text-align: center;
+    padding: 100px;
+}
+
+p { 
+    font-size: 25px;
+}
+
+h1 {
+    font-size: 80px;
+}
+
+</style>
--- QuizApp/src/views/QuizView.vue
+++ QuizApp/src/views/QuizView.vue
@@ -1,17 +1,70 @@
 <script setup>
 
-import { useRoute } from "vue-router"
-import quizes from "../data/data.json"
+    import Question from "../components/Question.vue"
+    import QuizHeader from "../components/QuizHeader.vue"
+    import Result from "../components/Result.vue"
+    import quizes from "../data/data.json"
+    import { useRoute } from "vue-router"
+    import { ref, watch, computed } from 'vue'
 
-const route = useRoute()
+    const route = useRoute()
+    const quizId = parseInt(route.params.id)
 
-const course = quizes.find(q => q.id == parseInt(route.params.id))
+    const quiz = quizes.find(q => q.id == quizId)
+    const currentQuestionIndex = ref(0)
+    const numberOfCorrectAnswers = ref(0)
+    const showResults = ref(false)
 
+    // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+    // watch(() => currentQuestionIndex.value, () => {
+    //     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    // })
+
+    const questionStatus = computed(() => {
+        return `${currentQuestionIndex.value}/${quiz.questions.length}`
+    })
+
+    const barPercentage = computed(() => {
+        return `${(currentQuestionIndex.value/quiz.questions.length)*100}%`
+    })
+
+    const onOptionSelected = (isCorrect) => {
+
+        console.log(isCorrect);
+
+        if (isCorrect) {
+            numberOfCorrectAnswers.value++
+        }
+
+        if (quiz.questions.length - 1 == currentQuestionIndex.value) {
+
+            showResults.value = true
+            
+        }
+
+        currentQuestionIndex.value++
+        
+
+    }
 
 </script>
 
 <template>
     <div>
-        <h1>Course is {{ course.name }}</h1>
+
+        <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage"/>
+        <div>
+            <Question 
+            :question="quiz.questions[currentQuestionIndex]" 
+            @selectOption="onOptionSelected"
+            v-if="!showResults"/>
+            <Result 
+                v-else
+                :quizQuestionLength="quiz.questions.length"
+                :numberOfCorrectAnswers="numberOfCorrectAnswers"
+            />
+        </div>
+    
     </div>
 </template>
